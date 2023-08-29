0e8aa832cf70457effbbc7b499013d51b892ed78
Arsen Rogovoi <arrog13@gmail.com>
2023-08-29 20:16:22 +0300

    jwt lib installed. authentification logic has built. added authentification logic in some routes and DAL module.

--- users/models/usersAccessData.js
+++ users/models/usersAccessData.js
@@ -6,6 +6,7 @@ const { handleBadRequest } = require("../../utils/errorHandler");
 const User = require("./mongodb/User");
 const lodash = require("lodash");
 const config = require("config");
+const { generateAuthToken } = require("../../auth/Providers/jwt");
 
 const DB = config.get("DB");
 
@@ -30,16 +31,17 @@ const registerUser = async (normalizedUser) => {
   return Promise.resolve("register not in mongodb");
 };
 
-const login = async ({ email, password }) => {
+const loginUser = async ({ email, password }) => {
   if (DB === "MONGODB") {
     try {
-      const user = User.findOne({ email });
+      const user = await User.findOne({ email });
       if (!user) throw new Error("Invalid email or password");
 
       const isPasswordValid = comparePassword(password, user.password);
       if (!isPasswordValid) throw new Error("Invalid email or password");
 
-      return Promise.resolve("user is logged in");
+      const token = generateAuthToken(user);
+      return Promise.resolve(token);
     } catch (error) {
       error.status = 400;
       return handleBadRequest("Mongoose", error);
@@ -48,5 +50,37 @@ const login = async ({ email, password }) => {
   return Promise.resolve({});
 };
 
-exports.login = login;
+const getUsers = async () => {
+  if (DB === "MONGODB") {
+    try {
+      const users = await User.find({}, { password: 0, __v: 0 });
+      return Promise.resolve(users);
+    } catch (error) {
+      error.status = 404;
+      return handleBadRequest("Mongoose", error);
+    }
+  }
+  return Promise.resolve("get users not in mongodb");
+};
+
+const getUser = async (userId) => {
+  if (DB === "MONGODB") {
+    try {
+      const user = await User.findById(userId, {
+        password: 0,
+        __v: 0,
+      });
+      if (!user) throw new Error("Could not find this user in the database");
+      return Promise.resolve(user);
+    } catch (error) {
+      error.status = 404;
+      return handleBadRequest("Mongoose", error);
+    }
+  }
+  return Promise.resolve("get user not in mongodb");
+};
+
+exports.loginUser = loginUser;
 exports.registerUser = registerUser;
+exports.getUsers = getUsers;
+exports.getUser = getUser;
--- users/routes/usersRestController.js
+++ users/routes/usersRestController.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const { handleError } = require("../../utils/errorHandler");
-const { registerUser, login } = require("../models/usersAccessData");
+const {
+  registerUser,
+  loginUser,
+  getUsers,
+  getUser,
+} = require("../models/usersAccessData");
 const {
   validateRegistration,
   validateLogin,
 } = require("../validations/userValidationService");
 const normalizeUser = require("../helpers/normalizeUser");
+const auth = require("../../auth/authService");
 
 router.post("/", async (req, res) => {
   try {
@@ -29,10 +35,45 @@ router.post("/login", async (req, res) => {
     const { error } = validateLogin(req.body);
     if (error)
       return handleError(res, 400, `Joi Error: ${error.details[0].message}`);
-    const user = await login(req.body);
+    const user = await loginUser(req.body);
     return res.send(user);
   } catch (error) {
     return handleError(res, error.status || 500, error.message);
+  }
+});
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const { isAdmin } = req.user;
+    if (!isAdmin)
+      return handleError(
+        res,
+        403,
+        "Authorization Error: You must be an admin user to see all users in the database"
+      );
+    const users = await getUsers();
+    return res.send(users);
+  } catch (error) {
+    const { status } = error;
+    return handleError(res, status || 500, error.message);
+  }
+});
+
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const { _id, isAdmin } = req.user;
+    const { id } = req.params;
+    if (_id !== id && !isAdmin)
+      return handleError(
+        res,
+        403,
+        "Authorization Error: You must be an admin type user or the registrated user to see this user"
+      );
+    const user = await getUser(id);
+    return res.send(user);
+  } catch (error) {
+    const { status } = error;
+    return handleError(res, status || 500, error.message);
   }
 });
 
